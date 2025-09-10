3303507ec74419598eb8180ef90e1888d247fe05
Richard Scarrott <riscarrott@googlemail.com>
2025-09-10 17:12:27 +0100

    Add reproduction for RR #14010

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,20 +1,23 @@
-import type { Route } from './+types/home';
+import type { Route } from "./+types/home";
 
 export function loader({ request }: Route.LoaderArgs) {
-  return { name: 'React Router', url: request.url };
+  return { name: "React Router", url: request.url };
 }
 
 export default function Home({ loaderData }: Route.ComponentProps) {
   return (
-    <div className="text-center p-4">
-      <h1 className="text-2xl">Hello, {loaderData.name}</h1>
-      URL: <p>{loaderData.url}</p>
-      <a
-        className="block mt-2 text-blue-500 underline hover:text-blue-600"
-        href="https://reactrouter.com/docs"
-      >
-        React Router Docs
-      </a>
+    <div className="mx-auto max-w-3xl">
+      <div className="text-center p-4 flex flex-col items-center gap-6">
+        <h1 className="text-2xl">Hello, {loaderData.name}</h1>
+        <div className="text-2xl">
+          URL: <span className="font-bold">{loaderData.url}</span>
+        </div>
+        {new URL(loaderData.url).hostname === "undefined" ? (
+          <div className="px-4 py-3 bg-red-700 text-white text-3xl ">Fail</div>
+        ) : (
+          <div className="px-4 py-3 bg-green-700 text-white text-3xl">Pass</div>
+        )}
+      </div>
     </div>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
-import { reactRouter } from '@react-router/dev/vite';
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import basicSsl from '@vitejs/plugin-basic-ssl';
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-  },
   plugins: [basicSsl(), tailwindcss(), reactRouter(), tsconfigPaths()],
 });
